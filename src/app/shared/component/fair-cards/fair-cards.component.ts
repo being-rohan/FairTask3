5bb3f87cc0bb285c829339121d81fa682126a34b
being-rohan <you@example.com>
2024-04-21 14:03:54 +0530

    okk

--- src/app/shared/component/fair-cards/fair-cards.component.ts
+++ src/app/shared/component/fair-cards/fair-cards.component.ts
@@ -11,6 +11,18 @@ export class FairCardsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let card = Array.from(document.getElementsByClassName('card'));
+    card[0].classList.add('active')
   }
+  setstyle(eve: HTMLDivElement) {
+    let card = Array.from(document.getElementsByClassName('card'));
+    card.map(ele => {
+      ele.classList.remove('active')
+    })
+    
+
+    eve.classList.add('active')
+  }
+
 
 }
